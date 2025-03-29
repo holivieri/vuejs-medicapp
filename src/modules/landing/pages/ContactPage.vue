921ad6e7d3757d591a08a40f64dbf19a62791e96
holivieri <hernan.olivieri@gmail.com>
2025-03-28 23:48:18 -0700

    Login

--- src/modules/landing/pages/ContactPage.vue
+++ src/modules/landing/pages/ContactPage.vue
@@ -1,3 +1,53 @@
 <template>
-  <h1>Contact Page</h1>
+  <!-- component -->
+  <section class="text-gray-600 body-font relative flex-1 h-full">
+    <div class="absolute inset-0 bg-gray-300">
+      <iframe
+        width="100%"
+        height="100%"
+        frameborder="0"
+        marginheight="0"
+        marginwidth="0"
+        title="map"
+        scrolling="no"
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4389.048295866128!2d-122.66102690845477!3d49.148489902883036!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x5485d199099099d3%3A0x3856cbc92eb58cac!2s20150%2081%20Ave%2C%20Langley%20Twp%2C%20BC%20V2Y%203L8!5e0!3m2!1ses!2sca!4v1743223535030!5m2!1ses!2sca"
+        style=""
+      ></iframe>
+    </div>
+    <div class="container px-5 py-24 mx-auto flex">
+      <div
+        class="lg:w-1/3 md:w-1/2 bg-white rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0 relative z-10 shadow-md"
+      >
+        <h2 class="text-gray-900 text-lg mb-1 font-medium title-font">Feedback</h2>
+        <p class="leading-relaxed mb-5 text-gray-600">
+          Post-ironic portland shabby chic echo park, banjo fashion axe
+        </p>
+        <div class="relative mb-4">
+          <label for="email" class="leading-7 text-sm text-gray-600">Email</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+          />
+        </div>
+        <div class="relative mb-4">
+          <label for="message" class="leading-7 text-sm text-gray-600">Message</label>
+          <textarea
+            id="message"
+            name="message"
+            class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
+          ></textarea>
+        </div>
+        <button
+          class="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg"
+        >
+          Button
+        </button>
+        <p class="text-xs text-gray-500 mt-3">
+          Chicharrones blog helvetica normcore iceland tousled brook viral artisan.
+        </p>
+      </div>
+    </div>
+  </section>
 </template>
