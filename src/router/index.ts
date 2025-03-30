5a1b78d9dd304347b750952a00d09c3cbda962f8
holivieri <hernan.olivieri@gmail.com>
2025-03-29 18:35:46 -0700

    login and signup

--- src/router/index.ts
+++ src/router/index.ts
@@ -34,7 +34,20 @@ export const router = createRouter({
     //Auth
     {
       path: '/auth',
-      component: () => import('@/modules/auth/pages/LoginPage.vue'),
+      redirect: '/login',
+      component: () => import('@/modules/auth/layouts/AuthLayout.vue'),
+      children: [
+        {
+          path: '/login',
+          name: 'login',
+          component: () => import('@/modules/auth/pages/LoginPage.vue'),
+        },
+        {
+          path: '/register',
+          name: 'register',
+          component: () => import('@/modules/auth/pages/RegisterPage.vue'),
+        },
+      ],
     },
   ],
 });
